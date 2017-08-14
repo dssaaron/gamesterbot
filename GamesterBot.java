024824337fee322f8fabe424ca424287f86bbf44
theone <6patok6patuwka@gmail.com>
2017-08-14 20:24:13 +0500

    Bot now can get message and read info from sender (long polling). No replies yet.

--- GamesterBot.java
+++ GamesterBot.java
@@ -2,15 +2,80 @@ package com.homedev.bot;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.TelegramBotAdapter;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.GetMe;
+import com.pengrad.telegrambot.request.GetUpdates;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.BaseResponse;
+import com.pengrad.telegrambot.response.GetMeResponse;
+import com.pengrad.telegrambot.response.GetUpdatesResponse;
+
+import java.util.List;
 
 public class GamesterBot {
 
+    static final String BOT_TOKEN = "404808549:AAEQGg_6K4W3JiDor3MIXCJOldYmLqV1sLw";
+
+    static TelegramBot bot = TelegramBotAdapter.build( BOT_TOKEN );
+
+    private static List<Update> GetUpdateList(int timeout, int offset) {
+
+        GetUpdates getUpdates = new GetUpdates().limit(5).offset(offset).timeout( timeout );
+        GetUpdatesResponse updatesResponse = bot.execute(getUpdates);
+        List<Update> updates = updatesResponse.updates();
+        return  updates;
+    }
+
     public static void main( String[] argc ) {
 
-        TelegramBot bot = TelegramBotAdapter.build( "404808549:AAEQGg_6K4W3JiDor3MIXCJOldYmLqV1sLw" );
-
         System.out.print( "STARTING BOT\n" );
-        Deck deck = new Deck( 36 );
-        deck.ShowDeck( );
+        //TelegramBot bot = TelegramBotAdapter.build( BOT_TOKEN );
+
+        //Getting the last worked update
+        //We need this to initialize getting message serviceß
+        int lastProcessedUpdateId;
+
+        //Getting old updates
+        List<Update> updates = GetUpdateList( 1, 0 );
+
+        //size of update's list
+        int sizeOfUpdates = updates.size();
+
+        //Setting the last update as worked
+        if ( sizeOfUpdates > 0 ) {
+
+            lastProcessedUpdateId = updates.get(sizeOfUpdates - 1).updateId();
+            System.out.printf("Last processed update id: %s\n", lastProcessedUpdateId);
+        } else {
+
+            lastProcessedUpdateId = 0;
+        }
+
+        //here we have bug in which there is infinite loop of first
+        //message if there were none of messages before bot started
+        do {
+
+            updates = GetUpdateList( 1, lastProcessedUpdateId + 1 );
+            sizeOfUpdates = updates.size();
+            if ( sizeOfUpdates > 0 ) {
+
+                for ( int iUpdate = 0; iUpdate < sizeOfUpdates; iUpdate++ ) {
+
+                    String message = updates.get( iUpdate ).message( ).text( ).toString( );
+                    lastProcessedUpdateId = updates.get( iUpdate ).updateId( );
+
+                    //System.out.println( updates.get( iUpdate ) );
+                    System.out.println( message );
+
+                    //trying to resend back message
+                    long chatId = updates.get( iUpdate ).message( ).chat( ).id( );
+                    SendMessage request = new SendMessage( chatId, message );
+                }
+            }
+        } while ( true );
+
+        //System.out.print( getUpdates );
+        //Deck deck = new Deck( 36 );
+        //deck.ShowDeck( );
     }
 }
--- Player.java
+++ Player.java
@@ -6,12 +6,15 @@ public class Player {
     private String name;
     private int cardAmount;
     private Card[] card; // somehow should get cards one by one even if there are more than 2 cards
+    private int balance;
 
     void SetId( int newId ) { id = newId; }
     void SetName( String newName ) { name = newName; }
     void SetCardAmount( int newCardAmount ) { cardAmount = newCardAmount; }
+    void SetBalance( int newBalance ) { balance = newBalance; }
 
     int GetId( ) { return id; }
     String GetName( ) { return name; }
     int GetCardAmount( ) { return cardAmount; }
+    int GetBalance( ) { return balance; }
 }
